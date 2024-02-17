ccb3c217139769efd796a55d48f551a7cd414494
kunaljaykam <kunaljaykam@gmail.com>
2024-02-18 03:03:13 +0530

    Tomcat uses dynamic version

--- config.sh
+++ config.sh
@@ -5,13 +5,15 @@
 # Path to the Sakai codebase directory
 SAKAI_CODEBASE_DIR="/home/kunaljaykam/sakaiproject/sakai"
 
-# Path to the Tomcat directory
-TOMCAT_DIR="/home/kunaljaykam/sakaiproject/apache-tomcat-9.0.85"
-
 # Path to the Sakai home directory
 SAKAI_HOME="/home/kunaljaykam/sakaiproject/sakai-home"
 
+# Tomcat version
+TOMCAT_VERSION="9.0.85"
+
+# Path to the Tomcat directory
+TOMCAT_DIR="/home/kunaljaykam/sakaiproject/apache-tomcat-$TOMCAT_VERSION"
+
 # URL to download Tomcat
-# Update this URL and Tomcat Path if you need a different version of Tomcat(https://tomcat.apache.org/download-90.cgi) 
-TOMCAT_URL="https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.85/bin/apache-tomcat-9.0.85.zip"
+TOMCAT_URL="https://dlcdn.apache.org/tomcat/tomcat-9/v$TOMCAT_VERSION/bin/apache-tomcat-$TOMCAT_VERSION.zip"
 
